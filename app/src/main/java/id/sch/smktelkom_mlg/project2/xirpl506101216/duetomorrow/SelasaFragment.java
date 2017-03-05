0312f144df080e013ddff0d1f27ea8198ba03fe0
GABS <galbie_elinour_24rpl@student.smktelkom-mlg.sch.id>
2017-03-05 14:47:32 +0700

    Commit First

---
+++ app/src/main/java/id/sch/smktelkom_mlg/project2/xirpl506101216/duetomorrow/SelasaFragment.java
@@ -0,0 +1,29 @@
+package id.sch.smktelkom_mlg.project2.xirpl506101216.duetomorrow;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SelasaFragment extends Fragment {
+
+
+    public SelasaFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_selasa, container, false);
+    }
+
+}
